5dc7ef93b934edffe382a9acd7df418a70b8d3dd
Relics <lcpingping@gmail.com>
2014-09-28 21:30:40 +0800

    简单棋盘+落子功能
    1. 代码已调通
    
    2. 吃子功能未实现
    
    3. 作研究代码用

---
+++ basicGame/basicGame/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace basicGame
+{
+    public partial class Form1 : Form
+    {
+        private Gobang gobang1;
+        private System.ComponentModel.IContainer components = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.gobang1 = new basicGame.Gobang();
+            this.SuspendLayout();
+            // 
+            // gobang1
+            // 
+            this.gobang1.BackColor = System.Drawing.Color.Wheat;
+            this.gobang1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gobang1.Horizontal = 19;
+            this.gobang1.Location = new System.Drawing.Point(10,10);
+            this.gobang1.Name = "gobang1";
+            this.gobang1.PaintLine = System.Drawing.Color.Black;
+            this.gobang1.Size = new System.Drawing.Size(581, 502);
+            this.gobang1.TabIndex = 0;
+            this.gobang1.Text = "gobang1";
+            this.gobang1.Vertical = 19;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(581, 502);
+            this.Controls.Add(this.gobang1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+        }
+    }
+}
---
+++ basicGame/basicGame/GobangArithmetic.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace basicGame
+{
+    public partial class Gobang
+    {
+        ChessPieces[] _Matrix;
+
+        private void InitMatrix()
+        {
+            _Matrix = new ChessPieces[this._horizontal * this._vertical];
+
+            for (int i = 0; i < _Matrix.Length; i++)
+            {
+                _Matrix[i] = new ChessPieces
+                {
+                    Color = Color.Black,
+                    IsOk = false,
+                    Step = 0
+                };
+            }
+        }
+    }
+}
