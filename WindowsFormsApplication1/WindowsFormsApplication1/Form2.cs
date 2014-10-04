e71887beee43c703b6bc8868266c67b5b2548d03
Relics <lcpingping@gmail.com>
2014-10-04 15:34:45 +0800

    A demo for the chessGameBasic interface

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+            pictureBox1.Visible = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int xCoordinate=MousePosition.X-this.Location.X;
+            int yCoordinate=MousePosition.Y-this.Location.Y;
+            if (xCoordinate > 200 && xCoordinate < 430 && yCoordinate > 510 && yCoordinate < 570) {
+                pictureBox1.Visible = true;
+            }
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            timer1.Interval = 1000;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
