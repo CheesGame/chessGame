e0c66524e92fea62ffd96535c149cac56f6bbfbc
Relics <lcpingping@gmail.com>
2014-10-05 22:29:13 +0800

    Algorithm_test
    C++FILE

---
+++ algorithm_test/algorithm_test/algorithm_test.cpp
@@ -0,0 +1,18 @@
+﻿//using C++
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+using namespace std;
+
+int main(){
+	char a[100];
+	int b[100];
+	int id=0;
+	while(true){
+		cin>>a;
+		if(strcmp(a,"?")==0) break;
+		else b[id++]=atoi(a);
+		//cout<<b[id-1]<<endl;
+	}
+	return 0;
+}
