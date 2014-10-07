7f3d0147d99fe970b98ca37185d27c9cf35acc5c
Relics <lcpingping@gmail.com>
2014-10-07 12:43:48 +0800

    moveInChess
    Main algorithm knocked by Chen LI

---
+++ Sgf/Test.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sgf
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            Step stepFile = new Step();
+            stepFile.fromFile("E://hehe.SGF");
+         //   stepFile.toString();
+            Console.ReadLine();
+            stepFile.saveToFile("E://TESTTT.SGF");
+            Console.ReadLine();
+            //Console.WriteLine(System.DateTime.Now);
+            //Console.ReadLine();
+        }
+    }
+}
